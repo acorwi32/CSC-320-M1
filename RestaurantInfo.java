267e794bc4284ba2c6ce7b67f4952d2aea161db7
acorwi32 <alexandrea.corwin@csuglobal.edu>
2025-04-20 20:05:59 -0500

    Add files via upload

---
+++ RestaurantInfo.java
@@ -0,0 +1,13 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        String restaurantName = "Narky Jalarky";
+        String businessAddress = "69100 T-Rex Avenue";
+        String city = "Mystic Falls";
+        String state = "VA";
+        String zipCode = "25986";
+
+        System.out.println(restaurantName);
+        System.out.println(businessAddress);
+        System.out.println(city + ", " + state + " " + zipCode);
+    }
+}
